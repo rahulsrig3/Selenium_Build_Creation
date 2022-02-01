81700d17c6484b798e60fb83af8e9bcbe8282cdc
rahulsrig3 <rsrivastava918@gmail.com>
2022-02-01 10:45:16 +0530

    This is my first commit for new Build

---
+++ src/test/java/selenium_Practice_BuildCreation/BuildCreationTest.java
@@ -0,0 +1,13 @@
+package selenium_Practice_BuildCreation;
+
+import org.testng.annotations.Test;
+
+public class BuildCreationTest {
+
+	@Test
+	
+	public void BuildCre01() {
+		
+		System.out.println("creating the build ");
+	}
+}
